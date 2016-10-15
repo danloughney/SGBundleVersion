3b3b9e16b8adccb223418ad39cf41e991c952157
Daniel Loughney <dan@spooky.group>
2016-10-15 09:27:56 -0400

    Initial Commit

---
+++ SGBundleVersion/SGBundleVersion.h
@@ -0,0 +1,19 @@
+//
+//  SGBundleVersion.h
+//  SGBundleVersion
+//
+//  Created by Daniel Loughney on 10/15/16.
+//  Copyright © 2016 SpookyGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SGBundleVersion.
+FOUNDATION_EXPORT double SGBundleVersionVersionNumber;
+
+//! Project version string for SGBundleVersion.
+FOUNDATION_EXPORT const unsigned char SGBundleVersionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SGBundleVersion/PublicHeader.h>
+
+
